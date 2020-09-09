c7115737ed3c38e42608a03658ff0678873b43e9
XiaoPan <2489964425@qq.com>
2020-09-09 17:23:50 +0800

    tabbar 页面结构，以及路由，样式配置完成

---
+++ src/pages/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+<view>
+  首页
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/pages/video/index.vue
@@ -0,0 +1,14 @@
+<template>
+<view>
+  精美
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+</style>
