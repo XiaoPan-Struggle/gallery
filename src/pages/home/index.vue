392ecfb198a0f3edd2f23a59fb728137d8970188
XiaoPan <2489964425@qq.com>
2020-09-09 21:12:38 +0800

    uni-ui 引入

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,14 +1,19 @@
 <template>
 <view>
   首页
+  <uni-badge text="1"></uni-badge>
 </view>
 </template>
 
 <script>
+import {
+  uniBadge
+} from '@dcloudio/uni-ui'
 export default {
-
+  components: {
+    uniBadge
+  },
 }
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
