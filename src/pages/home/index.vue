cac1608c8c61205613e4a4e78b47cce6b66826ad
XiaoPan <2489964425@qq.com>
2020-09-10 21:27:39 +0800

    首页的四个子页面创建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,19 +1,11 @@
 <template>
 <view>
   首页
-  <uni-badge text="1" type="success"></uni-badge>
 </view>
 </template>
 
 <script>
-import {
-  uniBadge
-} from '@dcloudio/uni-ui'
-export default {
-  components: {
-    uniBadge,
-  }
-}
+export default {}
 </script>
 
 <style lang="scss"></style>
