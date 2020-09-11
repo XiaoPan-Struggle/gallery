3808677ebc78fdc22f9a1893e9adf97a503a2931
XiaoPan <2489964425@qq.com>
2020-09-11 22:16:55 +0800

    代码优化

--- src/pages/home/home-recommend/index.vue
+++ src/pages/home/home-recommend/index.vue
@@ -1,5 +1,5 @@
 <template>
-<view>
+<view v-if="recommends.length > 0">
   <!-- 推荐 -->
   <view class="recommend_warp">
     <view class="recommend_item" v-for="item in recommends" :key="item.id">
@@ -8,19 +8,19 @@
   </view>
 
   <!-- 月份 -->
-  <view class="months_warp">
-    <view class="months_title">
-      <view class="months_title_info">
-        <view class="months_info">
+  <view class="monthes_warp">
+    <view class="monthes_title">
+      <view class="monthes_title_info">
+        <view class="monthes_info">
           <text>{{ months.DD }}</text>
           / {{ months.MM }} 月
         </view>
-        <view class="months_text">{{ months.title }}</view>
+        <view class="monthes_text">{{ months.title }}</view>
       </view>
-      <view class="months_title_more">更多 ></view>
+      <view class="monthes_title_more">更多 ></view>
     </view>
-    <view class="months_content">
-      <view class="months_item" v-for="item in months.items" :key="item.id">
+    <view class="monthes_content">
+      <view class="monthes_item" v-for="item in months.items" :key="item.id">
         <image mode="aspectFill" :src="item.thumb + item.rule.replace('$<Height>', 360)"></image>
       </view>
     </view>
@@ -58,7 +58,7 @@ export default {
       // 月
       this.months.MM = moment(this.months.stime).format('MM')
       this.months.DD = moment(this.months.stime).format('DD')
-      console.log(this.months)
+      console.log(result)
     })
   },
 }
@@ -77,44 +77,44 @@ export default {
 }
 
 // 月份
-.months_warp {
-  .months_title {
+.monthes_warp {
+  .monthes_title {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 20rpx;
 
-    .months_title_info {
+    .monthes_title_info {
       display: flex;
       color: $color;
       font-size: 30rpx;
       font-weight: 600;
 
-      .months_info {
+      .monthes_info {
         text {
           font-size: 36rpx;
         }
       }
 
-      .months_text {
+      .monthes_text {
         margin-left: 30rpx;
         color: #5b5b5b;
         font-size: 34rpx;
       }
     }
 
-    .months_title_more {
+    .monthes_title_more {
       color: $color;
       font-size: 24rpx;
       font-weight: 600;
     }
   }
 
-  .months_content {
+  .monthes_content {
     display: flex;
     flex-wrap: wrap;
 
-    .months_item {
+    .monthes_item {
       width: 33.3%;
       border: 5rpx solid #fff;
     }
