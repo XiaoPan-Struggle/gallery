ce38e7b7a9d2b2262efee991f5917b584fd2a7da
XiaoPan <2489964425@qq.com>
2020-09-11 22:11:34 +0800

    月份列表动态渲染

--- src/pages/home/home-recommend/index.vue
+++ src/pages/home/home-recommend/index.vue
@@ -2,51 +2,65 @@
 <view>
   <!-- 推荐 -->
   <view class="recommend_warp">
-    <view class="recommend_item" v-for="(item) in recommends" :key="item.id">
+    <view class="recommend_item" v-for="item in recommends" :key="item.id">
       <image mode="widthFix" :src="item.thumb"></image>
     </view>
   </view>
 
   <!-- 月份 -->
-  <view class="moneths_warp">
-    <view class="moneths_title">
-      <view class="moneths_title_info">
-        <view class="moneths_info">
-          <text>18</text>
-          / 01 月
+  <view class="months_warp">
+    <view class="months_title">
+      <view class="months_title_info">
+        <view class="months_info">
+          <text>{{ months.DD }}</text>
+          / {{ months.MM }} 月
         </view>
-        <view class="moneths_text">你负责美丽就好</view>
+        <view class="months_text">{{ months.title }}</view>
       </view>
-      <view class="moneths_title_more">更多 ></view>
+      <view class="months_title_more">更多 ></view>
     </view>
-    <view class="moneths_content"></view>
+    <view class="months_content">
+      <view class="months_item" v-for="item in months.items" :key="item.id">
+        <image mode="aspectFill" :src="item.thumb + item.rule.replace('$<Height>', 360)"></image>
+      </view>
+    </view>
   </view>
 </view>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   data() {
     return {
-      recommends: []
+      // 推荐
+      recommends: [],
+      // 月份
+      months: {},
     }
   },
   mounted() {
     this.request({
-        url: 'http://157.122.54.189:9088/image/v3/homepage/vertical',
-        data: {
-          // 获取多少条数据
-          limit: 30,
-          // 关键字
-          order: 'hot',
-          // 跳过几条
-          skip: 0
-        }
-      })
-      .then(result => {
-        this.recommends = result.res.homepage[1].items;
-      })
-  }
+      url: 'http://157.122.54.189:9088/image/v3/homepage/vertical',
+      data: {
+        // 获取多少条数据
+        limit: 30,
+        // 关键字
+        order: 'hot',
+        // 跳过几条
+        skip: 0,
+      },
+    }).then((result) => {
+      // 推荐
+      this.recommends = result.res.homepage[1].items
+      // 月份
+      this.months = result.res.homepage[2]
+      // 月
+      this.months.MM = moment(this.months.stime).format('MM')
+      this.months.DD = moment(this.months.stime).format('DD')
+      console.log(this.months)
+    })
+  },
 }
 </script>
 
@@ -63,39 +77,47 @@ export default {
 }
 
 // 月份
-.moneths_warp {
-  .moneths_title {
+.months_warp {
+  .months_title {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 20rpx;
 
-    .moneths_title_info {
+    .months_title_info {
       display: flex;
       color: $color;
       font-size: 30rpx;
       font-weight: 600;
 
-      .moneths_info {
+      .months_info {
         text {
           font-size: 36rpx;
         }
       }
 
-      .moneths_text {
+      .months_text {
         margin-left: 30rpx;
         color: #5b5b5b;
         font-size: 34rpx;
       }
     }
 
-    .moneths_title_more {
+    .months_title_more {
       color: $color;
       font-size: 24rpx;
       font-weight: 600;
     }
   }
 
-  .moneths_content {}
+  .months_content {
+    display: flex;
+    flex-wrap: wrap;
+
+    .months_item {
+      width: 33.3%;
+      border: 5rpx solid #fff;
+    }
+  }
 }
 </style>
