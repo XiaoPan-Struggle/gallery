53319ab73e48088532396295e35461737cae94d2
XiaoPan <2489964425@qq.com>
2020-09-12 17:12:12 +0800

    专辑背景数据渲染，样式

--- src/pages/album/index.vue
+++ src/pages/album/index.vue
@@ -1,6 +1,14 @@
 <template>
 <view>
-  专辑详情
+  <!--专辑背景-->
+  <view class="album_bg">
+    <image mode="aspectFill" :src="album.cover"></image>
+    <view class="album_info">
+      <view class="album_name">{{album.name}}</view>
+      <view class="album_btn">关注专辑</view>
+    </view>
+  </view>
+
 </view>
 </template>
 
@@ -13,6 +21,7 @@ export default {
         limit: 30,
         order: 'new',
         skip: 0,
+        // 下拉获取数据时，设置成0
         first: 1
       },
       // 接收页面参数id值
@@ -24,7 +33,8 @@ export default {
     }
   },
   onLoad(options) {
-    this.id = options.id
+    // this.id = options.id
+    this.id = '5e22780fe7bce739db126f29'
     this.getList()
     console.log(options.id)
   },
@@ -44,5 +54,37 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.album_bg {
+  position: relative;
+
+  image {
+    width: 100%;
+  }
+
+  .album_info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 80rpx;
+    padding: 30rpx;
+    font-size: 36rpx;
+    color: #fff;
+
+    .album_name {
+      font-weight: 600;
+    }
+
+    .album_btn {
+      padding: 10rpx 20rpx;
+      font-size: 24rpx;
+      border-radius: 10rpx;
+      background-color: $color;
+    }
+  }
+}
 </style>
