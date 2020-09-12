8ab739773cfc440f1429eab4544c8205b459fb5a
XiaoPan <2489964425@qq.com>
2020-09-12 16:42:58 +0800

    请求数据

--- src/pages/album/index.vue
+++ src/pages/album/index.vue
@@ -8,12 +8,32 @@
 export default {
   data() {
     return {
+      // 请求参数
+      params: {
+        limit: 30,
+        order: 'new',
+        skip: 0,
+        first: 1
+      },
+      // 接收页面参数id值
       id: -1
     }
   },
   onLoad(options) {
     this.id = options.id
+    this.getList()
     console.log(options.id)
+  },
+  methods: {
+    getList() {
+      this.request({
+          url: `http://157.122.54.189:9088/image/v1/wallpaper/album/${this.id}/wallpaper`,
+          data: this.params
+        })
+        .then(result => {
+          console.log(result)
+        })
+    }
   }
 }
 </script>
