24db462ee8cfa4b4383e75115e57d0bcd2cd593a
XiaoPan <2489964425@qq.com>
2020-09-13 19:40:52 +0800

    组件接收数据并缓存到全局

--- src/components/goDetail.vue
+++ src/components/goDetail.vue
@@ -1,13 +1,26 @@
 <template>
-<view>
-  组件
+<view @click="handleClick">
   <slot></slot>
 </view>
 </template>
 
 <script>
 export default {
-
+  props: {
+    list: Array,
+    index: Number
+  },
+  methods: {
+    handleClick() {
+      // 数据缓存
+      getApp().globalData.imgList = this.list
+      getApp().globalData.imgIndex = this.index
+      // 跳转路由
+      uni.navigateTo({
+        url: '/pages/imgDetail/index'
+      })
+    }
+  }
 }
 </script>
 
