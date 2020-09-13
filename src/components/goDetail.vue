5d58ca668736594edd404dd7434c5d083a0d8b2b
XiaoPan <2489964425@qq.com>
2020-09-13 19:30:01 +0800

    组件 和 图片详情页创建

---
+++ src/components/goDetail.vue
@@ -0,0 +1,15 @@
+<template>
+<view>
+  组件
+  <slot></slot>
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/pages/imgDetail/index.vue
@@ -0,0 +1,14 @@
+<template>
+<view>
+  图片详情
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+</style>
