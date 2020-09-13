334ea393504f9b3ff21246a8dbc1d1f09d675a55
XiaoPan <2489964425@qq.com>
2020-09-13 20:53:08 +0800

    高清大图渲染

--- src/pages/imgDetail/index.vue
+++ src/pages/imgDetail/index.vue
@@ -12,6 +12,11 @@
     </view>
   </view>
 
+  <!-- 发布的大图 -->
+  <view class="high_img">
+    <image mode="widthFix" :src="imgDetail.newThumb"></image>
+  </view>
+
 </view>
 </template>
 
@@ -28,10 +33,41 @@ export default {
       imgList
     } = getApp().globalData;
     this.imgDetail = imgList[imgIndex]
+    // 直接在路径上写入会报错，因为还没有拿到，路径会是undefined
+    this.imgDetail.newThumb = this.imgDetail.thumb + this.imgDetail.rule.replace('$<Height>', 360)
     console.log(this.imgDetail)
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.user_info {
+  display: flex;
+  padding: 30rpx;
+
+  .user_icon {
+    border-radius: 44rpx;
+    overflow: hidden;
+
+    image {
+      width: 88rpx;
+    }
+  }
+
+  .user_desc {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 30rpx;
+    font-size: 26rpx;
+
+    .user_name {
+      font-weight: 600;
+    }
+
+    .user_time {
+      color: #d3d3d3;
+    }
+  }
+}
 </style>
