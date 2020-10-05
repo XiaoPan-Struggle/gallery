6c441fc545e61d7b611f034309e042268df5035c
XiaoPan <2489964425@qq.com>
2020-10-05 17:48:37 +0800

    渲染分类页面

--- src/pages/home/home-category/index.vue
+++ src/pages/home/home-category/index.vue
@@ -1,12 +1,35 @@
 <template>
-<view>分类</view>
+<view class="category_list">
+  <view class="category_item" v-for="item in category" :key="item.id">
+    <image :src="item.cover"></image>
+    <view class="category name">{{ item.name }}</view>
+  </view>
+</view>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      // 分类数组
+      category: [],
+    }
+  },
+  mounted() {
+    this.getList()
+  },
+  methods: {
+    // 获取页面信息
+    getList() {
+      this.request({
+        url: 'http://157.122.54.189:9088/image/v1/vertical/category',
+      }).then((res) => {
+        this.category = res.res.category
+        console.log(this.category)
+      })
+    },
+  },
 }
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
