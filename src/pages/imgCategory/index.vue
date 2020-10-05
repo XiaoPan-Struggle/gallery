aafc4998dda14af7969c24651a109171d814ab03
XiaoPan <2489964425@qq.com>
2020-10-05 20:45:41 +0800

    页面渲染

--- src/pages/imgCategory/index.vue
+++ src/pages/imgCategory/index.vue
@@ -1,16 +1,19 @@
 <template>
 <view>
-  <view>
-    <view class="category_tab">
-      <view class="category_tab_title">
-        <view class="title_inner">
-          <uni-segmented-control :current="current" :values="items.map((v) => v.title)" @clickItem="onClickItem" style-type="text" active-color="#d4237a"></uni-segmented-control>
-        </view>
-        <view class="iconfont iconsearch"></view>
+  <view class="category_tab">
+    <view class="category_tab_title">
+      <view class="title_inner">
+        <uni-segmented-control :current="current" :values="items.map((v) => v.title)" @clickItem="onClickItem" style-type="text" active-color="#d4237a"></uni-segmented-control>
       </view>
-      <view class="content">
-        123
+      <view class="iconfont iconsearch"></view>
+    </view>
+    <view class="category_tabs_content" v-if="isshow">
+      <view class="category_item" v-for="item in vertical" :key="item.id">
+        <image :src="item.thumb" mode="widthFix"></image>
       </view>
+    </view>
+    <view class="category_not_data" v-else>
+      Not Data
     </view>
   </view>
 </view>
@@ -27,23 +30,45 @@ export default {
   data() {
     return {
       items: [{
-          title: '推荐',
+          title: '最新',
+          order: 'new',
         },
         {
-          title: '分类',
+          title: '热门',
+          order: 'hot',
         },
       ],
       current: 0,
+      params: {
+        limit: 30,
+        skip: 0,
+        order: 'new',
+      },
+      id: 0,
+      // 页面初始化数据
+      vertical: [],
+      isshow: true,
     }
+  },
+  onLoad(options) {
+    this.id = options.id
+    this.getList()
   },
   methods: {
     onClickItem(e) {
       if (this.current !== e.currentIndex) {
         this.current = e.currentIndex
-        uni.setNavigationBarTitle({
-          title: this.items[e.currentIndex].title,
-        })
       }
+      this.params.order = this.items[e.currentIndex].order
+      this.getList()
+    },
+    getList() {
+      this.request({
+        url: `http://157.122.54.189:9088/image/v1/vertical/category/${this.id}/vertical`,
+        data: this.params,
+      }).then((res) => {
+        this.vertical = res.res.vertical
+      })
     },
   },
 }
